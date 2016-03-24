4b6977107693054a93ecc9a7cd1a259986d4e828
Waseem John <johnwasim@gmail.com>
2016-03-24 16:14:09 +0500

    Reverting

--- Restartappservice.sh
+++
@@ -1,3 +0,0 @@
-ps -ef | awk '/appservices.jar/ && !/awk/ {print $2}' | xargs -r kill -9
-sleep 3
-java -Xms256M -Xmx1024M -XX:PermSize=64M -XX:MaxPermSize=256M -jar /home/ceaadmin-qa/deploy/appservices/appservices.jar >> /var/log/appservices/appservices.log &
--- Restartintservice.sh
+++
@@ -1,3 +0,0 @@
-ps -ef | awk '/intservices.jar/ && !/awk/ {print $2}' | xargs -r kill -9
-sleep 3
-java -Xms256M -Xmx1024M -XX:PermSize=64M -XX:MaxPermSize=256M -jar /home/ceaadmin-qa/deploy/intservices/intservices.jar >> /var/log/intservices/intservices.log &
